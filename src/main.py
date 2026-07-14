709c54d0b7b34caab385538fe4ea006283b51849
maudiibb <amrik@planberg.com>
2026-07-14 15:36:39 +0200

    Main skapad

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from src.file_scanner import list_files
+from src.metadata_extractor import extract_metadata
+import json
+
+def collect_all_metadata(root_path):
+    results = []
+    for file_path in list_files(root_path):
+        metadata = extract_metadata(file_path)
+        results.append(metadata)
+    return results
+
+def save_to_json(data, output_path):
+       with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
+if __name__ == "__main__":
+    resultat = collect_all_metadata(r"C:\Users\amrik\OneDrive\Desktop\Utbye")
+    save_to_json(resultat, "test_output.json")
+    print(f"Klart! {len(resultat)} poster sparade.")
